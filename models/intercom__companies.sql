e14351e63acd1a0c1bfb1c3644f6e66aad6a5c28
Fred Lennon <fred.lennon@payplan.com>
2026-01-14 14:11:40 +0000

    Removed reference to companies_plan

--- models/intercom__companies.sql
+++ models/intercom__companies.sql
@@ -1,15 +1,4 @@
-with companies_plan as (
-
-    select distinct
-        _AIRBYTE_RAW_ID,
-        plan_id,
-        plan_name
-    from {{ ref('stg_intercom__companies_plan') }}
-    group by _AIRBYTE_RAW_ID, plan_id, plan_name
-
-),
-
-companies as (
+with companies as (
 
     select
         company_id,
@@ -17,15 +6,12 @@ companies as (
         created_at_timestamp,
         industry,
         monthly_spend,
-        companies_plan.plan_id,
-        companies_plan.plan_name,
         session_count,
         updated_at_timestamp,
         user_count,
         website,
         _AIRBYTE_RAW_ID
     from {{ ref('int_intercom__latest_company') }}
-    left join companies_plan using (_AIRBYTE_RAW_ID)
 
 )
 
