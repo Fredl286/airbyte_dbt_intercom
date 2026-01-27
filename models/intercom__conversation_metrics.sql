47300c9ee2df9eeb38970df82a63a705bb04fdbe
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 14:42:19 +0000

    Join on metrics

--- models/intercom__conversation_metrics.sql
+++ models/intercom__conversation_metrics.sql
@@ -3,22 +3,32 @@ with conversations as (
     from {{ ref('intercom__conversations') }}
 ),
 
+part_aggregates as (
+    select *
+    from {{ ref('int_intercom__conversation_part_aggregates') }}
+),
+
 metrics as (
     select
-        conversation_id,
-        conversation_type,
-        conversation_state,
-        assignee_type,
-        sla_status,
-        conversation_rating,
-        last_closed_by_id,
-        datediff(day, created_at_timestamp, updated_at_timestamp) as days_open,
-        datediff(hour, created_at_timestamp, updated_at_timestamp) as hours_open,
-        count_total_parts,
-        count_reopens,
-        count_assignments,
-        time_to_first_response_minutes
-    from conversations
+        c.conversation_id,
+        c.conversation_type,
+        c.conversation_state,
+        c.assignee_type,
+        c.sla_status,
+        c.conversation_rating,
+        c.last_closed_by_id,
+
+        -- bring in aggregates
+        pa.total_parts as count_total_parts,
+        pa.first_part_date,
+        pa.last_part_date,
+
+        -- example metrics
+        datediff(day, c.created_at_timestamp, c.updated_at_timestamp) as days_open,
+        datediff(hour, c.created_at_timestamp, c.updated_at_timestamp) as hours_open
+    from conversations c
+    left join part_aggregates pa
+      on c.conversation_id = pa.conversation_id
 )
 
 select *
