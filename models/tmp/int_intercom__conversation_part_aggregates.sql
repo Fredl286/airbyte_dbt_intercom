dd062dd36cd406316221c78dbd7755a22c8d4bcd
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 15:12:03 +0000

    aggregates

--- models/tmp/int_intercom__conversation_part_aggregates.sql
+++ models/tmp/int_intercom__conversation_part_aggregates.sql
@@ -2,8 +2,8 @@ with conversation_parts as (
     select
         conversation_id,
         part_id,
-        AUTHOR:id::string as author_id,
-        AUTHOR:type::string as author_type,
+        author_id,
+        author_type,
         part_type,
         created_at_timestamp,
         updated_at_timestamp,
