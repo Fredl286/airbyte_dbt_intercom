809c25706dd7245beed909c919af3d40d8d058d1
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 11:24:10 +0000

    Removed quotes around AIRBYTE_RAW_ID

--- models/intercom__companies.sql
+++ models/intercom__companies.sql
@@ -10,7 +10,7 @@ with companies as (
         updated_at_timestamp,
         user_count,
         website,
-        "_AIRBYTE_RAW_ID" as raw_id   -- 👈 quoted and aliased
+        _AIRBYTE_RAW_ID as raw_id   -- 👈 quoted and aliased
     from {{ ref('int_intercom__latest_company') }}
 
 )
