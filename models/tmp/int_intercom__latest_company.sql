077203e1b25888ea9720579adbc10950ad41ccc1
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 11:28:04 +0000

    Amended intercom__latest_company.sql

--- models/tmp/int_intercom__latest_company.sql
+++ models/tmp/int_intercom__latest_company.sql
@@ -1,14 +1,35 @@
 with companies as (
-    select *
+
+    -- Pull in all the necessary fields from the staging model,
+    -- making sure to include _AIRBYTE_RAW_ID explicitly
+    select
+        _AIRBYTE_RAW_ID,
+        company_id,
+        company_name,
+        created_at_timestamp,
+        updated_at_timestamp,
+        industry,
+        monthly_spend,
+        session_count,
+        user_count,
+        website
     from {{ ref('stg_intercom__companies') }}
+
 ),
 
---Returns the most recent company record by creating a row number ordered by the updated_at_timestamp date, then filtering to only return the #1 row per company.
+-- Returns the most recent company record by creating a row number
+-- ordered by the updated_at_timestamp date, then filtering to only
+-- return the #1 row per company.
 latest_company as (
- select
-     *,
-         row_number() over(partition by company_id order by updated_at_timestamp desc) as latest_company_index
- from companies
+
+    select
+        companies.*,
+        row_number() over (
+            partition by company_id
+            order by updated_at_timestamp desc
+        ) as latest_company_index
+    from companies
+
 )
 
 select *
