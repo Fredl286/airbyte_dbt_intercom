4685046104a7b00fcfdafbaffe660848a9d7e6a1
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 12:46:18 +0000

    parts..

--- models/tmp/stg_intercom__conversation_parts.sql
+++ models/tmp/stg_intercom__conversation_parts.sql
@@ -3,8 +3,19 @@ with conversation_parts as (
     select
         id as part_id,
         conversation_id,
+
+        -- raw epoch values
+        created_at as created_at_epoch,
+        updated_at as updated_at_epoch,
+
+        -- converted to TIMESTAMP
         {{ dbt_date.from_unixtimestamp('created_at') }} as created_at_timestamp,
         {{ dbt_date.from_unixtimestamp('updated_at') }} as updated_at_timestamp,
+
+        -- also expose as *_date for downstream models that expect that alias
+        {{ dbt_date.from_unixtimestamp('created_at') }} as created_at_date,
+        {{ dbt_date.from_unixtimestamp('updated_at') }} as updated_at_date,
+
         author:id::string as author_id,
         body,
         _AIRBYTE_RAW_ID
