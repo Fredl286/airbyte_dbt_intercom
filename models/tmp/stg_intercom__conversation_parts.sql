7733c97f79e6209f2db4e5ee593e7211fe905b64
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 10:51:27 +0000

    fixed variable in conversation parts temp model

--- models/tmp/stg_intercom__conversation_parts.sql
+++ models/tmp/stg_intercom__conversation_parts.sql
@@ -4,7 +4,7 @@ select
     updated_at as updated_at_timestamp,
     {{ dbt_date.from_unixtimestamp('created_at') }} as created_at_date,
     {{ dbt_date.from_unixtimestamp('updated_at') }} as updated_at_date,
-    {{ fivetran_utils.json_parse(string="author", string_path=["id"]) }} as author_id,
-    {{ fivetran_utils.json_parse(string="author", string_path=["type"]) }} as author_type,
+    author:"id"::string as author_id,
+    author:"type"::string as author_type,
     *
 from {{ var('conversation_parts') }}
