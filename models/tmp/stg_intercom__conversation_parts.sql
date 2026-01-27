39c5c0bc5a75b0170995275c71a89576d0c9b9ec
Fred Lennon <fred.lennon@payplan.com>
2026-01-27 15:04:00 +0000

    Update the staging model to use uppercase AUTHOR:

--- models/tmp/stg_intercom__conversation_parts.sql
+++ models/tmp/stg_intercom__conversation_parts.sql
@@ -16,8 +16,11 @@ with conversation_parts as (
         {{ dbt_date.from_unixtimestamp('created_at') }} as created_at_date,
         {{ dbt_date.from_unixtimestamp('updated_at') }} as updated_at_date,
 
-        author:id::string as author_id,
+        AUTHOR:id::string as author_id,
+        AUTHOR:type::string as author_type,
+
         body,
+        part_type,
         _AIRBYTE_RAW_ID
     from {{ source('airbyte_intercom','conversation_parts') }}
 
