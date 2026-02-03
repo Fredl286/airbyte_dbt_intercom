527beb49d9cd979e626735b05231f59493d046fb
Fred Lennon <fred.lennon@payplan.com>
2026-02-03 14:22:01 +0000

    changed to vulcan_id

--- models/intercom_echo_conversations.sql
+++ models/intercom_echo_conversations.sql
@@ -8,21 +8,30 @@ tags_exploded as (
         r.id as conversation_id,
         t.value:"name"::string as tag_name,
         t.value:"applied_at"::bigint as applied_at_unix,
-        r.contacts,
-        r.custom_attributes
+        r.contacts
     from raw r,
          lateral flatten(input => r.tags:"tags") t
-    where t.value:"name"::string in ('Started ECHO Main', 'Completed ECHO main')
+    where t.value:"name"::string in ('Started ECHO Main', 'Completed ECHO Main')
+),
+
+-- join to contacts to get latest custom_attributes
+contacts as (
+    select
+        c.id as contact_id,
+        c.custom_attributes
+    from {{ source('airbyte_intercom','contacts') }} c
 ),
 
 transformed as (
     select
-        conversation_id,
-        tag_name,
-        to_char(to_timestamp(applied_at_unix), 'DD/MM/YYYY HH24:MI:SS') as applied_at,
-        coalesce(rtrim(custom_attributes:"Vulcan ID"::string), '') as vulcan_id,
-        contacts:"contacts"[0]:"id"::string as contact_id
-    from tags_exploded
+        te.conversation_id,
+        te.tag_name,
+        to_timestamp(te.applied_at_unix) as applied_at,
+        coalesce(rtrim(ct.custom_attributes:"vulcan_id"::string), '') as vulcan_id,
+        te.contacts:"contacts"[0]:"id"::string as contact_id
+    from tags_exploded te
+    left join contacts ct
+      on te.contacts:"contacts"[0]:"id"::string = ct.contact_id
 )
 
 select * from transformed
