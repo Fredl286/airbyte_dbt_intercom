51b2cf782bc5bed775200231926382e241609cc2
Fred Lennon <fred.lennon@payplan.com>
2026-02-03 14:32:52 +0000

    unflatten due to error

--- models/intercom_echo_conversations.sql
+++ models/intercom_echo_conversations.sql
@@ -8,12 +8,13 @@ tags_exploded as (
         r.id as conversation_id,
         t.value:"name"::string as tag_name,
         t.value:"applied_at"::bigint as applied_at_unix,
-        r.contacts:"contacts"[0]:"id"::string as contact_id
+        r.contacts
     from raw r,
          lateral flatten(input => r.tags:"tags") t
     where t.value:"name"::string in ('Started ECHO Main', 'Completed ECHO Main')
 ),
 
+-- join to contacts to get latest custom_attributes
 contacts as (
     select
         c.id as contact_id,
@@ -21,30 +22,16 @@ contacts as (
     from {{ source('airbyte_intercom','contacts') }} c
 ),
 
-pivoted as (
-    select
-        te.conversation_id,
-        te.contact_id,
-        max(case when te.tag_name = 'Started ECHO Main'
-                 then to_timestamp(te.applied_at_unix) end) as started_at,
-        max(case when te.tag_name = 'Completed ECHO Main'
-                 then to_timestamp(te.applied_at_unix) end) as completed_at,
-        listagg(te.tag_name, ', ') within group (order by te.applied_at_unix) as tags_applied
-    from tags_exploded te
-    group by te.conversation_id, te.contact_id
-),
-
 transformed as (
     select
-        p.conversation_id,
-        p.contact_id,
-        p.started_at,
-        p.completed_at,
-        p.tags_applied,
-        coalesce(rtrim(ct.custom_attributes:"vulcan_id"::string), '') as vulcan_id
-    from pivoted p
+        te.conversation_id,
+        te.tag_name,
+        to_timestamp(te.applied_at_unix) as applied_at,
+        coalesce(rtrim(ct.custom_attributes:"vulcan_id"::string), '') as vulcan_id,
+        te.contacts:"contacts"[0]:"id"::string as contact_id
+    from tags_exploded te
     left join contacts ct
-      on p.contact_id = ct.contact_id
+      on te.contacts:"contacts"[0]:"id"::string = ct.contact_id
 )
 
 select * from transformed
