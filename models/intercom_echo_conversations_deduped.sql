452220fb5eeb8cb3edf7330da71552a4cc39941e
Fred Lennon <fred.lennon@payplan.com>
2026-02-19 12:00:05 +0000

    Now deduping after conversation selection

--- models/intercom_echo_conversations_deduped.sql
+++ models/intercom_echo_conversations_deduped.sql
@@ -3,8 +3,6 @@
 with base as (
     select *
     from {{ ref('intercom_echo_conversations') }}
-    where email not ilike '%payplan.com%'
-      and email not ilike '%@test.com%'
 ),
 
 scored as (
@@ -24,16 +22,29 @@ ranked as (
             order by score desc, completed_at desc, started_at desc
         ) as rn
     from scored
+),
+
+deduped as (
+    select *
+    from ranked
+    where rn = 1
+),
+
+cleaned as (
+    select
+        conversation_id,
+        contact_id,
+        started_at,
+        completed_at,
+        tags_applied,
+        vulcan_id,
+        phone,
+        case
+            when email ilike '%payplan.com%' then null
+            when email ilike '%@test.com%' then null
+            else email
+        end as email
+    from deduped
 )
 
-select
-    conversation_id,
-    contact_id,
-    started_at,
-    completed_at,
-    tags_applied,
-    vulcan_id,
-    phone,
-    email
-from ranked
-where rn = 1
+select * from cleaned
