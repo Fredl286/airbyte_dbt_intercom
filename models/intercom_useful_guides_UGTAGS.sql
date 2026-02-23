054993505fa845911730ec48a99d2c593523b8b8
Fred Lennon <fred.lennon@payplan.com>
2026-02-23 14:25:43 +0000

    syntax correction around new custom attributes

--- models/intercom_useful_guides_UGTAGS.sql
+++ models/intercom_useful_guides_UGTAGS.sql
@@ -29,13 +29,11 @@ contacts as (
         nullif(rtrim(c.custom_attributes:"vulcan_id"::string), '') as vulcan_id,
         c.phone,
         c.email,
-
-        -- FORMAT TO 2 DECIMAL PLACES
         ROUND(TO_NUMBER(c.custom_attributes:"Surplus"::string), 2) as surplus,
         ROUND(TO_NUMBER(c.custom_attributes:"Vulcan Surplus"::string), 2) as vulcan_surplus,
         ROUND(TO_NUMBER(c.custom_attributes:"Total Unsecured Debt VSAPI"::string), 2) as total_unsecured_debt_vsapi,
-        ROUND(TO_NUMBER(c.custom_attributes:"Total Household Income"::string), 2) as total_unsecured_debt_vsapi,
-        ROUND(TO_NUMBER(c.custom_attributes:"Total Household Expenditure"::string), 2) as total_unsecured_debt_vsapi,
+        ROUND(TO_NUMBER(c.custom_attributes:"Total Household Income"::string), 2) as total_household_income,
+        ROUND(TO_NUMBER(c.custom_attributes:"Total Household Expenditure"::string), 2) as total_household_expenditure,
         ROUND(TO_NUMBER(c.custom_attributes:"total_debt"::string), 2) as total_debt
 
     from {{ source('airbyte_intercom','contacts') }} c
@@ -64,7 +62,8 @@ select
     ct.surplus,
     ct.vulcan_surplus,
     ct.total_unsecured_debt_vsapi,
-    ct.total_debt
+    ct.total_household_income,
+    ct.total_household_expenditure
 
 from pivoted p
 left join contacts ct
