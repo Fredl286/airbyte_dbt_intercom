2c9a2532507893034c50b063f47709fc76c0c98e
Fred Lennon <fred.lennon@payplan.com>
2026-02-23 12:54:04 +0000

    Useful guides added

---
+++ models/intercom_useful_guides.sql
@@ -0,0 +1,46 @@
+with raw as (
+    select *
+    from {{ source('airbyte_intercom','conversations') }}
+),
+
+tags_exploded as (
+    select distinct
+        r.id as conversation_id,
+        t.value:"name"::string as tag_name,
+        t.value:"applied_at"::bigint as applied_at_unix,
+        r.contacts:"contacts"[0]:"id"::string as contact_id
+    from raw r,
+         lateral flatten(input => r.tags:"tags") t
+    where t.value:"name"::string = 'Intercom Useful Guide'
+),
+
+contacts as (
+    select distinct
+        c.id as contact_id,
+        nullif(rtrim(c.custom_attributes:"vulcan_id"::string), '') as vulcan_id,
+        c.phone,
+        c.email
+    from {{ source('airbyte_intercom','contacts') }} c
+),
+
+pivoted as (
+    select
+        te.conversation_id,
+        te.contact_id,
+        max(to_timestamp(te.applied_at_unix)) as useful_guide_at,
+        listagg(distinct te.tag_name, ', ') as tags_applied
+    from tags_exploded te
+    group by te.conversation_id, te.contact_id
+)
+
+select
+    p.conversation_id,
+    p.contact_id,
+    p.useful_guide_at,
+    p.tags_applied,
+    ct.vulcan_id,
+    ct.phone,
+    ct.email
+from pivoted p
+left join contacts ct
+  on p.contact_id = ct.contact_id;
