cddb9e2518b81c15845924c8116b2f4bf9151907
Fred Lennon <fred.lennon@payplan.com>
2026-03-11 15:18:22 +0000

    further changes to CTE

--- models/intercom_echo_conversations.sql
+++ models/intercom_echo_conversations.sql
@@ -20,7 +20,15 @@ contacts as (
         c.id as contact_id,
         nullif(rtrim(c.custom_attributes:"vulcan_id"::string), '') as vulcan_id,
         c.phone,
-        c.email
+        c.email,
+
+        -- NEW FIELDS ADDED HERE
+        ROUND(TO_NUMBER(c.custom_attributes:"Surplus"::string), 2) as surplus,
+        ROUND(TO_NUMBER(c.custom_attributes:"Vulcan Surplus"::string), 2) as vulcan_surplus,
+        ROUND(TO_NUMBER(c.custom_attributes:"Total Unsecured Debt VSAPI"::string), 2) as total_unsecured_debt_vsapi,
+        ROUND(TO_NUMBER(c.custom_attributes:"Total Household Income"::string), 2) as total_household_income,
+        ROUND(TO_NUMBER(c.custom_attributes:"Total Household Expenditure"::string), 2) as total_household_expenditure
+
     from {{ source('airbyte_intercom','contacts') }} c
 ),
 
@@ -50,8 +58,7 @@ select
     ct.vulcan_surplus,
     ct.total_unsecured_debt_vsapi,
     ct.total_household_income,
-    ct.total_household_expenditure,
-
+    ct.total_household_expenditure
 from pivoted p
 left join contacts ct
   on p.contact_id = ct.contact_id
