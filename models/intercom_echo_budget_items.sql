c3958a7c9f1d66430706889690cf138777360c95
Fred Lennon <fred.lennon@payplan.com>
2026-03-25 10:15:12 +0000

    remove vulc surplus

--- models/intercom_echo_budget_items.sql
+++ models/intercom_echo_budget_items.sql
@@ -24,7 +24,6 @@ contacts as (
 
         -- EXISTING FIELDS
         ROUND(TO_NUMBER(c.custom_attributes:"Surplus"::string), 2) as surplus,
-        ROUND(TO_NUMBER(c.custom_attributes:"Vulcan Surplus"::string), 2) as vulcan_surplus,
         ROUND(TO_NUMBER(c.custom_attributes:"Total Unsecured Debt VSAPI"::string), 2) as total_unsecured_debt_vsapi,
         ROUND(TO_NUMBER(c.custom_attributes:"Total Household Income"::string), 2) as total_household_income,
         ROUND(TO_NUMBER(c.custom_attributes:"Total Household Expenditure"::string), 2) as total_household_expenditure,
@@ -106,7 +105,6 @@ select
     ct.phone,
     ct.email,
     ct.surplus,
-    ct.vulcan_surplus,
     ct.total_unsecured_debt_vsapi,
     ct.total_household_income,
     ct.total_household_expenditure,
