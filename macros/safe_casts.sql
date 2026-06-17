3f9bf35d99f8d86dcea6554bce054642005a469d
Fred Lennon <fred.lennon@payplan.com>
2026-06-17 14:56:47 +0000

    2dp macro added

--- macros/safe_casts.sql
+++ macros/safe_casts.sql
@@ -3,3 +3,13 @@
         REGEXP_REPLACE({{ field }}, '[^0-9.\-]', '')
     )
 {% endmacro %}
+
+
+{% macro safe_to_number_2dp(field) %}
+    ROUND(
+        TRY_TO_NUMBER(
+            REGEXP_REPLACE({{ field }}, '[^0-9.\-]', '')
+        ),
+        2
+    )
+{% endmacro %}
