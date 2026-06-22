56b844dcaaf470fc33c1d6fbaf623f83aaac3150
Fred Lennon <fred.lennon@payplan.com>
2026-06-22 13:24:48 +0000

    All tags table

--- models/intercom_conversations_alltags.sql
+++ models/intercom_conversations_alltags.sql
@@ -4,7 +4,7 @@ with raw as (
 ),
 
 tags_exploded as (
-    select
+    select distinct
         r.id as conversation_id,
         t.value:"name"::string as tag_name,
         t.value:"applied_at"::bigint as applied_at_unix,
@@ -14,15 +14,48 @@ tags_exploded as (
          lateral flatten(input => r.tags:"tags") t
 ),
 
--- optional: deduplicate in case of weird replays
-deduped as (
+-- ensure one row per contact_id
+contacts as (
     select distinct
-        conversation_id,
-        contact_id,
-        tag_name,
-        tag_applied_at
-    from tags_exploded
+        c.id as contact_id,
+
+        nullif(
+            trim(lower(c.custom_attributes:"vulcan_id"::string)),
+            ''
+        )::VARCHAR(50) as vulcan_id,
+
+        c.phone,
+        c.email,
+
+        ROUND(TRY_TO_NUMBER(c.custom_attributes:"Surplus"::string), 2) as surplus,
+        ROUND(TRY_TO_NUMBER(c.custom_attributes:"Vulcan Surplus"::string), 2) as vulcan_surplus,
+        ROUND(TRY_TO_NUMBER(c.custom_attributes:"Total Unsecured Debt VSAPI"::string), 2) as total_unsecured_debt_vsapi,
+        ROUND(TRY_TO_NUMBER(c.custom_attributes:"Total Household Income"::string), 2) as total_household_income,
+        ROUND(TRY_TO_NUMBER(c.custom_attributes:"Total Household Expenditure"::string), 2) as total_household_expenditure
+
+    from {{ source('airbyte_intercom','contacts') }} c
+),
+
+final as (
+    select
+        te.conversation_id,
+        te.contact_id,
+        te.tag_name,
+        te.tag_applied_at,
+
+        ct.vulcan_id,
+        ct.phone,
+        ct.email,
+        ct.surplus,
+        ct.vulcan_surplus,
+        ct.total_unsecured_debt_vsapi,
+        ct.total_household_income,
+        ct.total_household_expenditure
+
+    from tags_exploded te
+    left join contacts ct
+        on te.contact_id = ct.contact_id
 )
 
 select *
-from deduped
+from final
