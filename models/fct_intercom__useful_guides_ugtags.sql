e2ed12b13e078adc99eafa1918d156074145ef5d
Fred Lennon <fred.lennon@payplan.com>
2026-07-22 15:36:06 +0100

    consistent formatting in pound pence values, casting to 38,2 number always

--- models/fct_intercom__useful_guides_ugtags.sql
+++ models/fct_intercom__useful_guides_ugtags.sql
@@ -32,11 +32,11 @@ contacts as (
             c.phone,
             c.email,
 
-            {{ safe_to_number_38_0('c.custom_attributes:"Surplus"::string') }} as surplus,
-            {{ safe_to_number_38_0('c.custom_attributes:"Vulcan Surplus"::string') }} as vulcan_surplus,
-            {{ safe_to_number_38_0('c.custom_attributes:"Total Unsecured Debt VSAPI"::string') }} as total_unsecured_debt_vsapi,
-            {{ safe_to_number_38_0('c.custom_attributes:"Total Household Income"::string') }} as total_household_income,
-            {{ safe_to_number_38_0('c.custom_attributes:"Total Household Expenditure"::string') }} as total_household_expenditure,
+            {{ safe_to_number_38_2('c.custom_attributes:"Surplus"::string') }} as surplus,
+            {{ safe_to_number_38_2('c.custom_attributes:"Vulcan Surplus"::string') }} as vulcan_surplus,
+            {{ safe_to_number_38_2('c.custom_attributes:"Total Unsecured Debt VSAPI"::string') }} as total_unsecured_debt_vsapi,
+            {{ safe_to_number_38_2('c.custom_attributes:"Total Household Income"::string') }} as total_household_income,
+            {{ safe_to_number_38_2('c.custom_attributes:"Total Household Expenditure"::string') }} as total_household_expenditure,
 
             row_number() over (
                 partition by c.id
