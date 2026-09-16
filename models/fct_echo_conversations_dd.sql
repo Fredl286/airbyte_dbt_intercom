f72ec98011a29f45c8ea7a55143ae1c8490c64e0
Fred Lennon <fred.lennon@payplan.com>
2026-09-16 14:11:08 +0100

    Added new values regarding clean total debt

--- models/fct_echo_conversations_dd.sql
+++ models/fct_echo_conversations_dd.sql
@@ -13,7 +13,10 @@ with deduped as (
             'email',
             'surplus',
             'vulcan_surplus',
+            'total_debt_form',
+            'total_debt_with_ccjs',
             'total_unsecured_debt_vsapi',
+            'clean_total_debt',
             'total_household_income',
             'total_household_expenditure'
         ]
@@ -32,7 +35,10 @@ cleaned as (
         email,
         cast(surplus as number(38,0)) as surplus,
         cast(vulcan_surplus as number(38,0)) as vulcan_surplus,
-        cast(total_unsecured_debt_vsapi as number(38,0)) as total_unsecured_debt,
+        cast(total_debt_form as number(38,2)) as total_debt_form,
+        cast(total_debt_with_ccjs as number(38,2)) as total_debt_with_ccjs,
+        cast(total_unsecured_debt_vsapi as number(38,2)) as total_unsecured_debt_vsapi,
+        cast(clean_total_debt as number(38,2)) as clean_total_debt,
         cast(total_household_income as number(38,0)) as total_household_income,
         cast(total_household_expenditure as number(38,0)) as total_household_expenditure
     from deduped
